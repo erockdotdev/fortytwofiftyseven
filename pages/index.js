839b9b32a296d842644b013cbf56f955a8968b7d
Eric Sanchez <eric.q.sanchez@gmail.com>
2021-03-19 20:57:40 -0400

    develop test

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <h1>fortytwofiftyseven</h1>
+        <h1>develop-fortytwofiftyseven</h1>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
